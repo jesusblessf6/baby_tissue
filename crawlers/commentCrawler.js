4067d57f2cf92e60374cc26315bfb525efd64375
yirun0701 <yirun0701@icloud.com>
2014-02-25 17:37:49 +0800

    update

--- crawlers/commentCrawler.js
+++ crawlers/commentCrawler.js
@@ -2,6 +2,9 @@ exports.start = function(item, outercallback){
 	var async = require('async');
 	var webdriver = require('selenium-webdriver');
 	var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+	//console.log(item);
+	var moment = require('moment');
+	var Comment = require('../models/comment');
 
 	if(item.type == 'tmall'){
 		async.series([
@@ -10,7 +13,186 @@ exports.start = function(item, outercallback){
 			}, 
 
 			function(callback){
-				driver.get(item.url).then(setTimeout(callback, 10000));
+				driver.get(item.url).then(callback);
+			},
+
+			function(callback){
+				driver.get(item.url + "#bd").then(function(){
+					setTimeout(callback, 2000);
+				});
+			},
+
+			function(callback){
+				driver.get(item.url + "#J_TabBar").then(function(){
+					setTimeout(callback, 2000);
+				});
+			},
+
+			function(callback){
+				driver.findElement({id : "J_TabBar"}).then(function(tabBarDiv){
+					tabBarDiv.findElement({partialLinkText : "累计评价"}).then(function(commentLink){
+						commentLink.click();
+					});
+				}).then(function(){
+					setTimeout(callback, 3000);
+				});
+			},
+
+			function(callback){
+				driver.get(item.url + "#J_Reviews").then(function(){
+					setTimeout(callback, 3000);
+				});
+			},
+
+			function(callback){
+
+				driver.findElement({id : 'J_Reviews'}).then(function(revDiv){
+					revDiv.findElement({className : "tm-rate"}).then(function(rateDiv){
+						rateDiv.findElement({className : "rate-toolbar"}).then(function(rateToolbarDiv){
+							rateToolbarDiv.findElement({name : "content"}).then(function(ch){
+								ch.click();
+							});
+						});
+					});
+				}).then(function(){
+					setTimeout(callback, 3000);
+				});
+			},
+
+			function(callback){
+				var flag = true;
+				async.whilst(function(){
+					return flag;
+				}, function(callback){
+					async.series([
+						function(callback){
+							//get the comments from page
+							driver.findElement({className : "rate-grid"}).then(function(rateGrid){
+								rateGrid.findElements({tagName : "tr"}).then(function(trs){
+									async.eachSeries(trs, function(tr, callback){
+										var tmpcomment = {};
+										async.series([
+											function(callback){
+												//get the comment content
+												tr.findElement({className : "tm-rate-fulltxt"}).then(function(fulltext){
+													
+													fulltext.getText().then(function(x){
+														tmpcomment.content = x;
+													});
+												}).then(callback);
+											},
+
+											function(callback){
+												//get the comment time
+												tr.findElement({className : "tm-rate-date"}).then(function(cdate){
+													cdate.getText().then(function(cdt){
+														var today = new Date();
+														var today_year = today.getYear();
+														var today_month = today.getMonth();
+														var today_day = today.getDay();
+														today.setHours(0);
+														today.setMinutes(0);
+														today.setSeconds(0);
+														today.setMilliseconds(0);
+														if(cdt == '今天'){	
+															tmpcomment.publishDate = today;
+														}else{
+															tmpcomment.publishDate = moment(today_year.toString() + "."+cdt, "YYYY.MM.DD");
+														}
+													});
+												}).then(callback);
+											},
+
+											function(callback){
+												//sku info
+												tr.findElement({className : 'rate-sku'}).then(function(rateSku){
+													rateSku.getText().then(function(rst){
+														tmpcomment.spec = rst;
+													});
+												}).then(callback);
+											},
+
+											function(callback){
+												//customer info
+												tr.findElement({className : "rate-user-info"}).then(function(cusEle){
+													cusEle.isElementPresent({tagName : 'a'}).then(function(p){
+														if(p){
+															cusEle.findElement({tagName : 'a'}).then(function(cuslink){
+																cuslink.getAttribute("href").then(function(h){
+																	tmpcomment.cusLink = h;
+																}).then(function(){
+																	cuslink.getText().then(function(x){
+																		tmpcomment.cusName = x;
+																	});
+																});
+															}).then(function(){
+																tmpcomment.isAnonymous = false;
+															});
+														}else{
+															cusEle.getText().then(function(x){
+																tmpcomment.cusName = x;
+															}).then(function(){tmpcomment.isAnonymous = true;});
+														}
+													});
+												}).then(callback);
+											},
+
+											function(callback){
+												//save the comment
+												var c = new Comment(tmpcomment);
+												c.save(function(err, result){
+													if(err){
+														console.log(err);
+													}
+													callback();
+												});
+											}
+										], function(err){
+											if(err){
+												console.log(err);
+											}
+											callback();
+										});
+									}, function(err){
+										if(err){
+											console.log(err);
+										}
+									});
+								});
+							}).then(callback);
+						},
+
+						function(callback){
+							//check if there is the next page link
+							//if true, then click the next page link
+							//else set the fla to false
+							driver.findElement({className : 'rate-paginator'}).then(function(pagefooter){
+								pagefooter.isElementPresent({partialLinkText : '下一页'}).then(function(p){
+									if(p){
+										pagefooter.findElement({partialLinkText : '下一页'}).then(function(nextpage){
+											nextpage.click();
+										});
+									}else{
+										flag = false;
+									}
+								});
+							}).then(function(){
+								setTimeout(callback, 3000);
+							});
+						}
+					], function(err){
+						if(err){
+							console.log(err);
+						}
+						callback();
+					});
+				}, function(err){
+					if(err){
+						console.log(err);
+					}
+					callback();
+				});
+				
 			},
 
 			function(callback){
--- crawlers/tradeCrawler.js
+++ crawlers/tradeCrawler.js
@@ -1,6 +1,13 @@
 exports.start = function(item, outercallback){
 	var async = require('async');
 	var webdriver = require('selenium-webdriver');
+	//var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+
+	if(item.type == 'tmall'){
+
+	}else{
+
+	}
 
 	outercallback();
 };
